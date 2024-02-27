5cd2d4a4814344744e79d14ab952740d40f42434
Nnamdi Michael Okpala <koodu-platform@protonmail.com>
2024-02-27 20:29:34 +0000

    a

--- gosilang/src/include/tokens.h
+++ gosilang/src/include/tokens.h
@@ -4,12 +4,17 @@
 #include "token.h"
 
 type(TokenKeywordType,
+    KW_CONST,
+    KW_LET,
+
     KW_IF,
     KW_ELSE_IF,
     KW_ELSE,
-
     KW_WHILE,
+)
 
+value(TokenKeywordValue,
+    char* data; // Common value for most token types
 )
 
 
