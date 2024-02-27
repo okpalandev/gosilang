1247180d2763f5f35aea2d73ab86005649407854
Nnamdi Michael Okpala <koodu-platform@protonmail.com>
2024-02-27 15:53:15 +0000

    a

---
+++ gosilang/src/include/symbol_table.h
@@ -0,0 +1,12 @@
+#ifndef SYMBOL_TABLE_H
+#define SYMBOL_TABLE_H
+
+
+// A SymbolTable to lookup value given a MinHeap and MaxHeap.
+// using a two-pointer technique.
+typedef struct SymbolTable_s SymbolTable;
+typedef struct SymbolTable_s {
+
+};
+
+#endif // SYMBOL_TABLE_H
