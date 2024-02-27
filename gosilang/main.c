14b0e61c8bb53238c53414079f533777282cff4a
Nnamdi Michael Okpala <koodu-platform@protonmail.com>
2024-02-27 21:48:12 +0000

    a

--- gosilang/main.c
+++ gosilang/main.c
@@ -1,29 +1,37 @@
-
 #include "tokenizer.h"
 #include "token.h"
 
-int main() {
-    // Initialize the tokenizer
-    Tokenizer_t *tokenizer = Tokenizer_init(NULL, NULL, 1000);
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+        return 1;
+    }
 
-    // Read input from the user or from a file
-    char input[1000]; // Adjust the size according to your needs
-    printf("Enter input: ");
-    fgets(input, sizeof(input), stdin);
+    FILE *fp = fopen(argv[1], "r");
+    if (fp == NULL) {
+        fprintf(stderr, "Error opening file %s\n", argv[1]);
+        return 1;
+    }
+
+    // Initialize the tokenizer
+    Tokenizer_t *tokenizer = Tokenizer_init(NULL, NULL, 20);
 
     // Tokenize the input
-    Token_t *tokens = tokenize(tokenizer, input);
-
-    // Process or print the tokens
-    if (tokens != NULL) {
-        printf("Tokens:\n");
-        printf("Type: %d, Value: %s\n", tokens->type, tokens->value.data);
+    Token_t *token;
+    char input[1000]; // Adjust the size according to your needs
+    while (fgets(input, sizeof(input), fp) != NULL) {
+        token = tokenize(tokenizer, input);
+        // Process or print the tokens
+        if (token != NULL) {
+            printf("Tokens:\n");
+            printf("Type: %d, Value: %s\n", token->type, token->value.data);
+        }
     }
 
     // Free memory
     Tokenizer_free(tokenizer);
-    Token_free(tokens);
+    Token_free(token);
 
+    fclose(fp);
     return 0;
 }
-
