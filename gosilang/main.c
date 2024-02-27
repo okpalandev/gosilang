5ff4709a269cf9a82935ebbe5a63bdb02a86b1bc
Nnamdi Michael Okpala <koodu-platform@protonmail.com>
2024-02-27 23:36:19 +0000

    a

--- gosilang/main.c
+++ gosilang/main.c
@@ -27,20 +27,24 @@ int main(int argc, char *argv[]) {
 
     Token_t *token;
     char input[1000];
+    // Inside your main function where you tokenize the input
     while (fgets(input, sizeof(input), fp) != NULL) {
-        // Tokenize the input
+    // Tokenize the input
         token = tokenize(tokenizer, input);
 
-        // Process or add tokens to the symbol table
+    // Process or add tokens to the symbol table
         while (token != NULL) {
-            // Add the token to the symbol table
-            if (token->type == TOKEN_IDENTIFIER) {
-                SymbolTable_add(table, token->value.data, token->type, token->value);
+        // Check if the token is the 'oru' keyword
+            if (token->type == TOKEN_KEYWORD && strcmp(token->value.data, "oru") == 0) {
+            // Add 'oru' to the symbol table
+            SymbolTable_add(table, "oru", token->type, token->value);
             }
-            // Tokenize the next input line
+
+        // Tokenize the next input line
             token = tokenize(tokenizer, NULL);
         }
     }
+
 
 
     Tokenizer_free(tokenizer);
