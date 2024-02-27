c21904c51302d832d96467c8fbbf2c6e01df264a
Nnamdi Michael Okpala <koodu-platform@protonmail.com>
2024-02-27 15:34:15 +0000

    a

--- gosilang/src/include/AST.h
+++ gosilang/src/include/AST.h
@@ -1,4 +1,16 @@
 #ifndef AST_H
 #define AST_H
 
+#include "tokenizer.h"
+
+
+typedef struct ASTNode_s ASTNode;
+struct ASTNode_s {
+};
+
+typedef struct AST_s AST;
+struct AST_s {
+
+};
+
 #endif // AST_H
---
+++ gosilang/src/include/heap.h
@@ -0,0 +1,26 @@
+#ifndef HEAP_H
+#define HEAP_H
+
+
+// Most Compiler Architecure only support top-down
+// top-to-bottom parsing.
+//Consequently,Due to nature of quatum computers their
+// exist an effiecent to read both mutiple tapes
+// yielding mutiple result of the computations.
+
+// This module HEAP_H consist of a MaxHeap ,and MinHeap
+// used in symbol_table.h to optimize the algorithm
+// lookup.
+
+
+typedef struct MinHeap_s MinHeap;
+struct MinHeap_s {
+
+}
+
+typedef struct MaxHeap_s MaxHeap;
+struct MaxHeap_s {
+
+}
+
+#endif // HEAP_H
