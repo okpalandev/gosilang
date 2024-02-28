2c2ccdc4a61f3c9b8267190854bcc006b90aea91
Nnamdi Michael Okpala <koodu-platform@protonmail.com>
2024-02-28 00:23:27 +0000

    a

--- gosilang/src/token.c
+++ gosilang/src/token.c
@@ -1,7 +1,6 @@
 #include "indirection.h"
 #include "token.h"
 
-
 Token_t* Token_init(TokenType *type, TokenValue *value) {
     Token_t *token = malloc(sizeof(Token_t));
     if (token == NULL) {
@@ -11,36 +10,44 @@ Token_t* Token_init(TokenType *type, TokenValue *value) {
 
     // Initialize type and value if pointers are not NULL
     if (type != NULL) {
-        token->type = type;
+        token->type = type; // Dereference the pointer to access the enum value
     } else {
-        // Handle the case when type pointer is NULL
-        token->type = TOKEN_INVALID; // Or set to a default value
+        // Handle the case when type pointer is NULL;
+        token->type = TOKEN_INVALID; // set to a default value.
     }
 
     if (value != NULL) {
+        // Allocate memory for token->value and token->value->data
+        token->value = malloc(sizeof(TokenValue));
+        if (token->value == NULL) {
+            fprintf(stderr, "Failed to allocate memory for Token value");
+            free(token);
+            return NULL;
+        }
         token->value->data = malloc(strlen(value->data) + 1);
         if (token->value->data == NULL) {
             fprintf(stderr, "Failed to allocate memory for Token value data");
-            Token_free(token);
+            free(token->value); // Free allocated memory before returning NULL
+            free(token);
             return NULL;
         }
         strcpy(token->value->data, value->data);
     } else {
         // Handle the case when value pointer is NULL
         // For example, set token->value->data to NULL
-        token->value->data = NULL;
+        token->value = NULL;
     }
 
     return token;
 }
 
-
-
-
 void Token_free(Token_t *token) {
     if (token != NULL) {
-        free(token->value->data);
+        // Free allocated memory for token->value and token->value->data
+        if (token->value != NULL) {
+            free(token->value->data);
+            free(token->value);
+        }
         free(token);
-
     }
 }
